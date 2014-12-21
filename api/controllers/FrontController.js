2098e266f14ebe5dbee0649dee06c4a5271f918a
freakyzoidberg <whatafreakyzoidberg@gmail.com>
2014-12-21 00:05:16 +0000

    #3 / is replied with a 404, maybe need a controller

---
+++ api/controllers/FrontController.js
@@ -0,0 +1,11 @@
+/**
+ * FrontController
+ *
+ * @description :: Dummy Server-side logic for managing main page
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+var passport = require('passport');
+module.exports = {
+
+};
