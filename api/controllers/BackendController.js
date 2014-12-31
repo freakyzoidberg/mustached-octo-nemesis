21af71a381a99be8ce5da9e7917b75a19bd61832
Pierre <freakyzoidberg@users.noreply.github.com>
2014-12-31 12:10:09 +0000

    Update BackendController.js

--- api/controllers/BackendController.js
+++ api/controllers/BackendController.js
@@ -7,13 +7,13 @@
 
 module.exports = {
 findAll : function(req, res) {
-		Backend.find().exec(function(err, backends) {
-			if (!backends) {
-				return res.notAcceptable({err: 'User does not exists'});
-			}
-			return res.ok({
-				backends: backends
-			});
-		});
-	}
+    Backend.find().exec(function(err, backends) {
+      if (!backends) {
+        return res.notAcceptable({err: 'User does not exists'});
+      }
+      return res.ok({
+        backends: backends
+      });
+    });
+  }
 };
