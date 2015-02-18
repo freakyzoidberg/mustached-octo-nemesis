9c2295dfed324b30e83f8e7bbda96670219a4532
freakyzoidberg <pierre@lacave.me>
2015-02-18 20:43:17 +0000

    no overide palette

--- assets/js/app.js
+++ assets/js/app.js
@@ -47,10 +47,10 @@ angular
         "default": 'retro'  // Mystery man as default for missing avatars
       };
       gravatarServiceProvider.secure = true;
-      $mdThemingProvider.theme('default')
-        .primaryPalette('teal')
-        .accentPalette('amber')
-        .warnPalette('pink');
+      // $mdThemingProvider.theme('default')
+      //   .primaryPalette('teal')
+      //   .accentPalette('amber')
+      //   .warnPalette('pink');
 
         //Available palettes: red, pink, purple, deep-purple, indigo, blue, light-blue, cyan, teal, green, light-green, lime, yellow, amber, orange, deep-orange, brown, grey, blue-grey
         //theme.primaryPalette, theme.accentPalette, theme.warnPalette, theme.backgroundPalette
